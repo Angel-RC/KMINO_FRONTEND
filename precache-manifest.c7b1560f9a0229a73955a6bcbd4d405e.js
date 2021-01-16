e3a24c5967c1d00caa1260273a12f5c5dabbf882
Angel <angel.r.chicote@gmail.com>
2021-01-16 10:46:10 +0100

    Updates

---
+++ precache-manifest.c7b1560f9a0229a73955a6bcbd4d405e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f795d8a416b096e785322670c08a5d1f",
+    "url": "/KMINO_FRONTEND/index.html"
+  },
+  {
+    "revision": "5995f72927db26a100a4",
+    "url": "/KMINO_FRONTEND/static/css/2.6a46f5ef.chunk.css"
+  },
+  {
+    "revision": "5ddc3430a839549354bb",
+    "url": "/KMINO_FRONTEND/static/css/main.b881e8e2.chunk.css"
+  },
+  {
+    "revision": "5995f72927db26a100a4",
+    "url": "/KMINO_FRONTEND/static/js/2.ccba3594.chunk.js"
+  },
+  {
+    "revision": "5ddc3430a839549354bb",
+    "url": "/KMINO_FRONTEND/static/js/main.8846ce4c.chunk.js"
+  },
+  {
+    "revision": "20e954492aa7183f4fd2",
+    "url": "/KMINO_FRONTEND/static/js/runtime-main.7ce1c073.js"
+  },
+  {
+    "revision": "65ae4bca9d967835b3dcedd122e4c30d",
+    "url": "/KMINO_FRONTEND/static/media/plantilla_destinos.65ae4bca.csv"
+  },
+  {
+    "revision": "1a7e27e9bb72f1db5946363225dcf707",
+    "url": "/KMINO_FRONTEND/static/media/plantilla_fleet.1a7e27e9.csv"
+  },
+  {
+    "revision": "bd6f36a3a214069dcdcb241e049d6b6d",
+    "url": "/KMINO_FRONTEND/static/media/venue_location_icon.bd6f36a3.svg"
+  }
+]);
